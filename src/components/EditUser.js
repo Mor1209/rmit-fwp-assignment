9fbe20495ef4fd79faed52024ffc5e12e94bfff1
moritzHauptmann <mohauptmann@gmail.com>
2022-08-14 21:57:13 +1000

    adding delete user functionality

--- src/components/EditUser.js
+++ src/components/EditUser.js
@@ -1,27 +1,59 @@
-import { IconButton, Stack } from '@mui/material'
+import { Button, IconButton, Stack, Typography, Box } from '@mui/material'
 import React, { useState } from 'react'
 import EditUserForm from './Forms/EditUserForm'
 import BasicModal from './UI/BasicModal'
 import DeleteIcon from '@mui/icons-material/Delete'
 import EditIcon from '@mui/icons-material/Edit'
+import { deleteUser } from '../data/users'
+import { useAuthContext } from '../hooks/useAuthContext'
 
 const EditUser = () => {
-  const [modalIsOpen, setModalIsOpen] = useState(false)
+  const { logout } = useAuthContext()
+  const [editIsOpen, setEditIsOpen] = useState(false)
+  const [deleteIsOpen, setDeleteIsOpen] = useState(false)
 
-  const handleModalToggle = () => setModalIsOpen(!modalIsOpen)
+  const handleEditToggle = () => setEditIsOpen(!editIsOpen)
+  const handleDeleteToggle = e => {
+    if (e.target.id === 'confirm') {
+      deleteUser()
+      logout()
+    }
+
+    setDeleteIsOpen(!deleteIsOpen)
+  }
 
   return (
     <>
       <Stack direction="row">
-        <IconButton aria-label="edit" onClick={handleModalToggle}>
+        <IconButton aria-label="edit" onClick={handleEditToggle}>
           <EditIcon color="primary" />
         </IconButton>
-        <IconButton aria-label="delete" onClick={handleModalToggle}>
+        <IconButton aria-label="delete" onClick={handleDeleteToggle}>
           <DeleteIcon color="error" />
         </IconButton>
       </Stack>
-      <BasicModal open={modalIsOpen} close={handleModalToggle}>
-        <EditUserForm onSuccess={handleModalToggle} />
+      <BasicModal open={editIsOpen} close={handleEditToggle}>
+        <EditUserForm onSuccess={handleEditToggle} />
+      </BasicModal>
+      <BasicModal open={deleteIsOpen} close={handleDeleteToggle}>
+        <Box sx={{ padding: 3 }}>
+          <Typography variant="h5">
+            Are you sure you want to delete your account ?
+          </Typography>
+          <Stack
+            direction="row"
+            justifyContent="flex-end"
+            spacing={1}
+            sx={{ mt: 4 }}
+          >
+            <Button id="confirm" onClick={handleDeleteToggle} color="error">
+              Confirm
+            </Button>
+            <Button id="cancel" onClick={handleDeleteToggle}>
+              Cancel
+            </Button>
+          </Stack>
+        </Box>
       </BasicModal>
     </>
   )
