6d0a4aec2f05c397535228b29e2be7b0a338a609
moritzHauptmann <mohauptmann@gmail.com>
2023-01-21 11:12:42 +1100

    change env variable names for db

--- Rest-Api/config/config.cjs
+++ Rest-Api/config/config.cjs
@@ -2,11 +2,11 @@ require('dotenv').config()
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    port: process.env.DB_PORT,
-    host: process.env.DB_HOST,
+    username: process.env.MYSQLUSER,
+    password: process.env.MYSQLPASSWORD,
+    database: process.env.MYSQLDATABASE,
+    port: process.env.MYSQLPORT,
+    host: process.env.MYSQLHOST,
     dialect: 'mysql',
   },
   test: {
